4018fb115a7be019f1739786a30a7c2bb25bfc75
jajet.loh <jajetloh@gmail.com>
2022-04-24 11:44:15 +0800

    initial commit with dynamic formulas working

---
+++ src/lib/dynamic-formulas.spec.ts
@@ -0,0 +1,101 @@
+import test from 'ava'
+
+import { solveEquations, DynamicFormulaSolver, Equation, SolverVariable } from './dynamic-formulas'
+
+const TEST_EQUATIONS = [
+    {
+        name: 'a=b+c+d',
+        variables: ['a', 'b', 'c', 'd'],
+        equations: {
+            a: x => x.b + x.c + x.d,
+            b: x => x.a - x.c - x.d,
+            c: x => x.a - x.b - x.d,
+            d: x => x.a - x.b - x.c,
+        },
+    },
+    {
+        name: 'a=e*f',
+        variables: ['a', 'e', 'f'],
+        equations: {
+            a: x => x.e * x.f,
+            e: x => x.a / x.f,
+            f: x => x.a / x.e,
+        },
+    },
+]
+
+test('solveEquations', (t) => {
+    const equations: Equation[] = TEST_EQUATIONS
+    const inputs = {
+        e: 3,
+        f: 5,
+        b: 2,
+        c: 6,
+    }
+    const result = solveEquations(equations, inputs)
+    const expectedResult = {
+        a: 15,
+        b: 2,
+        c: 6,
+        d: 7,
+        e: 3,
+        f: 5,
+    }
+    t.deepEqual(result, expectedResult)
+})
+
+test('DynamicFormulaSolver', (t) => {
+    const solver = new DynamicFormulaSolver(
+        { a: { name: 'a' }, b: { name: 'b' }, c: { name: 'c' }, d: { name: 'd' }, e: { name: 'e' }, f: { name: 'f' } },
+        TEST_EQUATIONS,
+        4,
+    )
+
+    const result = solver.refreshNumbers({
+        e: 3,
+        f: 5,
+        b: 2,
+    }, ['c', 6])
+
+    const expectedVariables: { [k: string]: SolverVariable } = {
+        a: {
+            checkValue: false,
+            checkDisabled: true,
+            fieldValue: 15,
+            fieldDisabled: true,
+        },
+        b: {
+            checkValue: true,
+            checkDisabled: false,
+            fieldValue: 2,
+            fieldDisabled: false,
+        },
+        c: {
+            checkValue: false,
+            checkDisabled: true,
+            fieldValue: 6,
+            fieldDisabled: false,
+        },
+        d: {
+            checkValue: false,
+            checkDisabled: true,
+            fieldValue: 7,
+            fieldDisabled: false,
+        },
+        e: {
+            checkValue: true,
+            checkDisabled: false,
+            fieldValue: 3,
+            fieldDisabled: false,
+        },
+        f: {
+            checkValue: true,
+            checkDisabled: false,
+            fieldValue: 5,
+            fieldDisabled: false,
+        },
+    }
+
+    t.deepEqual(result.variables, expectedVariables)
+})
+
---
+++ src/lib/dynamic-formulas.ts
@@ -0,0 +1,92 @@
+interface Variable {
+    name: string
+}
+
+export interface Equation {
+    name: string
+    variables: string[]
+    equations: { [varName: string]: (x: any) => number }
+}
+
+interface SolverWarning {
+    type: string
+    message: string
+}
+
+export interface SolverVariable {
+    checkDisabled: boolean
+    checkValue: boolean
+    fieldDisabled: boolean
+    fieldValue: number | null
+}
+
+interface SolverResult {
+    variables: { [k: string]: SolverVariable }
+    warnings: SolverWarning[]
+}
+
+export class DynamicFormulaSolver {
+
+    constructor(
+        private readonly variables: { [k: string]: Variable },
+        private equations: Equation[],
+        private degreesOfFreedom: number,
+    ) {
+        this.validateInputs()
+    }
+
+    private validateInputs() { }
+
+    refreshNumbers(lockedValues: { [k: string]: number }, lastChanged?: [string, number]): SolverResult {
+        let results = solveEquations(this.equations, lockedValues)
+
+        const valuesForcedByLockValues = Object.entries(results)
+            .filter(([k, _]) => !(k in lockedValues))
+            .reduce((acc, [k, v]: [string, number]) => { acc[k] = v; return acc }, {} as { [k: string]: number })
+
+        if (lastChanged) {
+            const [changedVar, changedValue] = lastChanged
+            results = solveEquations(this.equations, { ...results, [changedVar]: changedValue })
+        }
+
+        const forcedValuesAll = Object.entries(results)
+            .filter(([k, _]) => !(k in lockedValues))
+            .reduce((acc, [k, v]: [string, number]) => { acc[k] = v; return acc }, {} as { [k: string]: number })
+
+        const maxLocksReached = Object.keys(lockedValues).length >= this.degreesOfFreedom - 1
+
+        return {
+            variables: Object.keys(this.variables).map(v => [
+                v,
+                {
+                    checkValue: v in lockedValues,
+                    checkDisabled: v in valuesForcedByLockValues || (maxLocksReached && !(v in lockedValues)),
+                    fieldValue: forcedValuesAll[v] ?? (lockedValues[v] ?? null),
+                    fieldDisabled: v in valuesForcedByLockValues,
+                } as SolverVariable
+            ]).reduce((acc, [k, v]: [string, SolverVariable]) => { acc[k] = v; return acc }, {}),
+            warnings: [] as SolverWarning[],
+        } as SolverResult
+    }
+}
+
+export function solveEquations(equations: Equation[], inputs: { [k: string]: number }, maxDepth: number = 50): { [k: string]: number } {
+    const results = { ...inputs }
+    let previousKnownCount = -1
+    let knownCount = Object.keys(results).length
+    let counter = 0
+    while (counter < maxDepth) {
+        previousKnownCount = knownCount
+        equations.forEach(eqn => {
+            const unknowns = eqn.variables.filter((v: string) => !(v in results))
+            if (unknowns.length === 1) {
+                const vNew: string = unknowns[0]
+                results[vNew] = (eqn as any).equations[vNew](results)
+            }
+        })
+        knownCount = Object.keys(results).length
+        if (previousKnownCount == knownCount) break
+        counter++
+    }
+    return results
+}
