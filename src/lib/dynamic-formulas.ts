6f2a99e3b3fa4f5da00dab8c55eaa46aaf171d00
jajet.loh <jajetloh@gmail.com>
2022-04-26 22:34:14 +0800

    add support for validating completeness of sets of equations

--- src/lib/dynamic-formulas.ts
+++ src/lib/dynamic-formulas.ts
@@ -2,6 +2,7 @@ export interface Equation {
     name: string
     variables: string[]
     eqArrangements: { [varName: string]: (x: any) => number }
+    auxiliary?: boolean
 }
 
 interface SolverWarning {
@@ -89,6 +90,7 @@ export function solveEquations(equations: Equation[], inputs: { [k: string]: num
 
 export interface EquationValidationResult {
     isValid: boolean,
+    badVariableCombinations: string[][],
     equations: {
         name: string,
         variables: string[],
@@ -98,11 +100,14 @@ export interface EquationValidationResult {
     }[]
 }
 
-export function validateEquations(equations: Equation[], inputs: { [k: string]: number }): EquationValidationResult {
+export function validateEquations(variables: string[], equations: Equation[], inputs: { [k: string]: number }): EquationValidationResult {
     const equationResults: EquationValidationResult = {
         isValid: false,
+        badVariableCombinations: [],
         equations: [],
     }
+    const coreEquations: Equation[] = equations.filter(x => !x.auxiliary)
+    const degreesOfFreedom = variables.length - coreEquations.length
     equations.forEach(eqn => {
         const [valuesFromEquations, correctValues]: [{ [k: string]: number }, { [k: string]: boolean }] = eqn.variables.map(k => [k, eqn.eqArrangements[k]]).reduce((acc, [k, fn]: [string, (x: any) => number]) => {
             const filteredInputs = Object.entries(inputs).filter(([k, _]) => eqn.variables.includes(k)).reduce((acc, [k, v]) => { acc[k] = v; return acc }, {})
@@ -120,9 +125,80 @@ export function validateEquations(equations: Equation[], inputs: { [k: string]: 
         })
     })
 
-    if (equationResults.equations.every(eqnResult => Object.values(eqnResult.eqArrangementsValid).every(x => x) && eqnResult.variablesInArrangementsValid)) {
+
+    for (const combination of generateCombinations(variables, degreesOfFreedom)) {
+        const knowns = [...combination]
+        let previousKnownCount = -1
+        let isOverconstrained = false
+        const coreEquationVars: string[][] = coreEquations.map(eqn => eqn.variables)
+
+        // Check if this set of variables in 'combination' overconstrains the equation
+        while (previousKnownCount !== knowns.length && !isOverconstrained) {
+            previousKnownCount = knowns.length
+            coreEquationVars.forEach((eqnVars, i, a) => {
+                const eqnUnknowns = eqnVars.filter(v => !knowns.includes(v))
+                if (eqnUnknowns.length === 1) {
+                    knowns.push(eqnUnknowns[0])
+                    a.splice(i, 1)
+                } else if (eqnUnknowns.length === 0) {
+                    isOverconstrained = true
+                }
+            })
+        }
+
+        if (isOverconstrained) continue
+
+        // If not overconstrained, check to see if it can be solved
+        const knowns2 = [...combination]
+        let previousKnownCount2 = -1
+        while (previousKnownCount2 !== knowns2.length) {
+            previousKnownCount2 = knowns2.length
+            equations.forEach(eqn => {
+                const eqnUnknowns = eqn.variables.filter(v => !knowns2.includes(v))
+                if (eqnUnknowns.length === 1) knowns2.push(eqnUnknowns[0])
+            })
+        }
+
+        if (knowns2.length < variables.length) equationResults.badVariableCombinations.push([...combination])
+    }
+
+    if (equationResults.equations.every(eqnResult => Object.values(eqnResult.eqArrangementsValid).every(x => x) && eqnResult.variablesInArrangementsValid)
+        && equationResults.badVariableCombinations.length === 0) {
         equationResults.isValid = true
     }
 
     return equationResults
+}
+
+// From https://stackoverflow.com/a/61418166
+function generateCombinations(sourceArray, comboLength) {
+    const sourceLength = sourceArray.length;
+    if (comboLength > sourceLength) return [];
+
+    const combos = []; // Stores valid combinations as they are generated.
+
+    // Accepts a partial combination, an index into sourceArray, 
+    // and the number of elements required to be added to create a full-length combination.
+    // Called recursively to build combinations, adding subsequent elements at each call depth.
+    const makeNextCombos = (workingCombo, currentIndex, remainingCount) => {
+        const oneAwayFromComboLength = remainingCount == 1;
+
+        // For each element that remaines to be added to the working combination.
+        for (let sourceIndex = currentIndex; sourceIndex < sourceLength; sourceIndex++) {
+            // Get next (possibly partial) combination.
+            const next = [...workingCombo, sourceArray[sourceIndex]];
+
+            if (oneAwayFromComboLength) {
+                // Combo of right length found, save it.
+                combos.push(next);
+            }
+            else {
+                // Otherwise go deeper to add more elements to the current partial combination.
+                makeNextCombos(next, sourceIndex + 1, remainingCount - 1);
+            }
+        }
+    }
+
+    makeNextCombos([], 0, comboLength);
+    return combos;
 }
